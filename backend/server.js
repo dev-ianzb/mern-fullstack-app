04134ceb5a2af856c82d5b86b26b53e7ac40e70d
dev-ianzb <dev.ianzb@gmail.com>
2025-11-01 11:37:51 +0800

    Initial commit and also created the back end API's and monggoDB database connections

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //allows us to accept json data in body
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server running at http://localhost:" + PORT);
+});
