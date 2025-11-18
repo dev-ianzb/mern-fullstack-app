424a720e669d98d6bb5b859253831dbe728b92e6
dev-ianzb <dev.ianzb@gmail.com>
2025-11-18 21:11:58 +0800

    added UI for displaying products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,49 @@
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Container maxW={"container.xl"} py={12}>
+        <VStack spacing={8}>
+          <Text
+            fontSize={"30"}
+            fontWeight={"bold"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+            textAlign={"center"}
+          >
+            Current Products 🚀
+          </Text>
+
+          <SimpleGrid
+            columns={{ base: 1, md: 3, lg: 3 }}
+            spacing={10}
+            w={"full"}
+          ></SimpleGrid>
+
+          <Text
+            fontSize="xl"
+            textAlign={"center"}
+            fontWeight="bold"
+            color="gray.500"
+          >
+            No products found 😢{" "}
+            <Link to={"/create"}>
+              <Text
+                as="span"
+                color="blue.500"
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        </VStack>
+      </Container>
+    </div>
+  );
 };
 
 export default HomePage;
